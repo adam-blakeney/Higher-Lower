c81c073168a0d792d5fcd9739e086763054df594
adam-blakeney <adam.blakeney@outlook.com>
2022-04-26 15:30:39 +0000

    after tutor help, trying using data types ,with exaples

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,155 @@
-/*jshint esversion: 6 */
-// declared variables
-const playerPicksDisplay = document.querySelector('#player-picks');
-const compPicksDisplay= document.querySelector('#comp-picks');
+const computerChoiceDisplay = document.querySelector('#computer-choice');
+const userChoiceDisplay = document.querySelector('#user-choice');
 const resultDisplay = document.querySelector('#result');
-const playerScore = document.querySelector('#player-score');
-const compScore = document.querySelector('#comp-score');
+const possibleChoices = document.querySelectorAll('.button');
+const win = document.querySelector('.win');
+const lose = document.querySelector('.lose');
+const openModal = document.querySelector('#open');
+const modalContainer = document.querySelector('#modal_container');
+const closeModal = document.querySelector('#close');
+const dark = document.querySelector('#dark');
+const element = document.body;
+let userChoice;
+let computerChoice;
+let result;
+let userscore = 0;
+let computerscore = 0;
+let turns = 0;
 
-const button = document.querySelectorAll('button');
-let playerPicks;
-let compPicks;
-let result; 
-let userScore = 0;
-let machineScore = 0;
 
-/**
- * main game function
- */
-button.forEach(button => button.addEventListener('click', function() {
-    playerPicks = this.id;
-    playerPicksDisplay.innerHTML = playerPicks;
-    compRandomAnswer();
-    getResult();
-    gameOver();
-    reStart();
+/*
+This is the main function. It puts the buttons into a array and adds an eventlistener that waits for the users
+clicks and then calls upon the the other functions when programed.
+*/
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', function() {
+   userChoice = this.id;
+   userChoiceDisplay.innerHTML = userChoice;
+   generateComputerChoice();
+   getResult();
+   countDown();
+   
+   
+   
+   // Calls the gameOver function after 5 turns
+   if(turns == 5) {
+      gameOver(possibleChoices, countDown);
+   }
+   
+
 }));
 
 /**
- * fuction generates ranodom number
+ * This generates a random number between 1 - 3 for the computer by giving 
+ * them the value of Rock, Paper and Scissors. Which will then be displayed 
+ * to the User.
  */
-function compRandomAnswer() {
-    const randomNumber = Math.floor(Math.random() * button.length + 1);
-    console.log(randomNumber);
-
-    if (randomNumber === 1) {
-        compPicks = 'rock';
-    } else if (randomNumber === 2) {
-        compPicks = 'scissors';
-    } else if (randomNumber === 3) {
-        compPicks = 'paper';
-    }
-
-    compPicksDisplay.innerHTML = compPicks;
+function generateComputerChoice() {
+   const randomNumber = Math.floor(Math.random() * 3) + 1;
+   
+   if (randomNumber === 1) {
+      computerChoice = 'rock';
+   }
+   if (randomNumber === 2) {
+      computerChoice = 'paper';
+   }
+   if (randomNumber === 3) {
+      computerChoice = 'scissors';
+   }
+   computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 /**
- * function to determine the winner
+ * This checkes the players choice(the buttons) with the computers choice(generateComputerChoice)
+ * to determine the winer and loser. Then will increment either scores by 1.
  */
 function getResult() {
-    if (compPicks === playerPicks) {
-        result = 'go again...';
-    } else if (compPicks === 'rock' && playerPicks === "paper") {
-        result = 'winner!';
-        userScore++;
-        playerScore.textContent = userScore;
-
-    } else if (compPicks === 'rock' && playerPicks === "scissors") {
-        result = 'oops..';
-        machineScore++;
-        compScore.textContent = machineScore;
-    } else if (compPicks === 'paper' && playerPicks === "scissors") {
-        result = 'you won!';
-        userScore++;
-        playerScore.textContent = userScore;
-    } else if (compPicks === 'paper' && playerPicks === "rock") {
-        result = 'loser..';
-        machineScore++;
-        compScore.textContent = machineScore; 
-    } else if (compPicks === 'scissors' && playerPicks === "rock") {
-        result = 'easy!';
-        userScore++;
-        playerScore.textContent = userScore;
-    } else if (compPicks === 'scissors' && playerPicks === "paper") {
-        result = 'you lose...';
-        machineScore++;
-        compScore.textContent = machineScore;
-    }
-
-    resultDisplay.innerHTML = result;
-     
+   
+   if (computerChoice === userChoice) {
+      result = 'Tie';
+   }else if(userChoice === 'rock') {
+      if(computerChoice === 'scissors') {
+         result = 'you win';
+         userscore++; // increments user score by 1 if won
+         win.innerHTML = userscore;
+      }else {
+         result = 'you lose';
+         computerscore++; // increments computers score by 1 if user lost
+         lose.innerHTML = computerscore;
+      }
+   }
+   else if(userChoice === 'paper') {
+      if(computerChoice === 'rock') {
+         result = 'you win';
+         userscore++;
+         win.innerHTML = userscore;
+      }else{
+         result = 'you lose';
+         computerscore++;
+         lose.innerHTML = computerscore;
+      }
+   }
+   else if(userChoice === 'scissors') {
+      if(computerChoice === 'paper') {
+         result = 'you win';
+         userscore++;
+         win.innerHTML = userscore;
+      }else{
+         result = 'you lose';
+         computerscore++;
+         lose.innerHTML = computerscore;
+      }
+   }
+   resultDisplay.innerHTML = result;
 }
 
 /**
- * function declares the winner and resets the game
+ * This tells the user how many turns they have left by listening for the 
+ * users clicks of the buttons on the page.
  */
-function gameOver(getResult) {
-    if (userScore === 3) {
-        alert('You are the Winner');
-        location.reload();
-    } else if (machineScore === 3) {
-        alert('You have lost...');
-        location.reload();
-    } 
-    
-} 
+function countDown() {
+   const turnsLeft = document.querySelector('.turns-left');
+   turns++; // incrementes the turns by 1
+   turnsLeft.innerHTML = 5-turns; // Displays turns left 
+}
 
 /**
-  function for reset button. refreshe page.
+ * This will check the users score and the computers score to determine the 
+ * winner of the game and display it to the user.
  */
-function reStart() {
-   const reStart = document.querySelector('#reset').addEventListener('click', function() {
-        window.location.reload();
-   });
-  
+function gameOver(possibleChoices, countDown) {
+   if(userscore > computerscore) {
+      alert('YOU WON THE GAME!');
+      location.reload();
+   }else if(userscore < computerscore) {
+      alert('YOU LOST GAME!');
+      location.reload();
+   }else {
+      alert("IT'S A TIE!");
+      location.reload();
+   }
+
 }
+
+/**
+ * This is the reload page function for the reload button at the 
+ * bottom of the HTML page.
+ */
+function refresh(possibleChoices){
+   window.location.reload("Refresh");
+}
+
+// Show the Modal Box when you click the RULES button
+openModal.addEventListener('click', () => {
+   modalContainer.classList.add('show');
+});
+
+// Closes the Modal Box when you click the CLOSE button inside.
+closeModal.addEventListener('click', () => {
+   modalContainer.classList.remove('show');
+});
+
+// Turns on background colour of the black when you click the DARK MODE button
+dark.addEventListener('click', () => {
+   element.classList.toggle('dark-mode');
+});
